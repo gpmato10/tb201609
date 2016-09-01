7ed5590927c2e95cf4793b7be7cac162efb2224f
dw <gpmato10@gmail.com>
2016-09-02 04:41:33 +0900

    p59 END.

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,70 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+/**
+ * Created by dw on 2016. 9. 2..
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tb","root","123123");
+
+        PreparedStatement preparedStatement = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tb","root","123123");
+
+        PreparedStatement preparedStatement = c.prepareStatement(
+                "select * from users where id = ?");
+        preparedStatement.setString(1, id);
+
+        ResultSet rs = preparedStatement.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        preparedStatement.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "이란 아이디 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "라는 아이디를 조회 성공함.");
+    }
+}
---
+++ src/main/java/springbook/user/domain/User.java
@@ -0,0 +1,34 @@
+package springbook.user.domain;
+
+/**
+ * Created by dw on 2016. 9. 2..
+ */
+public class User {
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
